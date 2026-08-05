8eb53c537f8a6f6ce98a2d9b0b4ed1fedd0102b8
Wilmer Bailon <Wilmerbailon007@gmail.com>
2026-08-06 00:33:55 +0200

    The code

---
+++ vase-generator/script.js
@@ -0,0 +1,14 @@
+import { animate, rebuildVase } from "./scene.js";
+import { drawProfile, initEditor } from "./ui.js";
+
+// Crear el primer jarrón
+rebuildVase();
+
+// Dibujar el perfil
+drawProfile();
+
+// Activar el editor
+initEditor();
+
+// Iniciar renderizado
+animate();
