02d1bdfbd29958e28690e8c2d074bd4dc2d99b5b
Wilmer Bailon <Wilmerbailon007@gmail.com>
2026-08-06 19:48:59 +0200

    added vite.config.js

--- vase-generator/vite.config.js
+++ vase-generator/vite.config.js
@@ -4,5 +4,12 @@ export default defineConfig({
   server: {
     port: 5173,
     open: true
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT,
+    allowedHosts: [
+      'clayforge-jar-project.onrender.com'
+    ]
   }
 });
